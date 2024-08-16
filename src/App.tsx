feebb4bb7f12b02fc6dc433183815477dcb57c87
OskarMartens <martens.oskar@gmail.com>
2024-08-16 14:02:34 +0200

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useRef, useState } from "react";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
-import { IDetailedDrink, IDrinkContext } from "./interfaces/interface";
 
 function App() {
 
